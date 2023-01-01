ccff4c3a581565fd94bc44306067873059a940bf
saahil <saahil.agrawal20@gmail.com>
2023-01-01 13:41:08 +0530

    Updated till 10 TextViews with ArrayList! Next, Recycling will be executed!

--- app/src/main/java/com/example/android/miwok/MainActivity.java
+++ app/src/main/java/com/example/android/miwok/MainActivity.java
@@ -15,17 +15,69 @@
  */
 package com.example.android.miwok;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
 // import android.support.v7.app.AppCompatActivity;
 import androidx.appcompat.app.AppCompatActivity;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         // Set the content of the activity to use the activity_main.xml layout file
         setContentView(R.layout.activity_main);
+        TextView numbers = findViewById(R.id.numbers);
+        TextView family = findViewById(R.id.family);
+        TextView colors = findViewById(R.id.colors);
+        TextView phrases = findViewById(R.id.phrases);
+        numbers.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Context context = getApplicationContext();
+                Toast.makeText(context, "Opening Numbers Page", Toast.LENGTH_SHORT).show();
+                Intent intent = new Intent(context, NumbersActivity.class);
+                startActivity(intent);
+            }
+        });
+        family.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Context context = getApplicationContext();
+                Toast.makeText(context, "Opening Family Page", Toast.LENGTH_SHORT).show();
+                Intent intent = new Intent(context, FamilyActivity.class);
+                startActivity(intent);
+            }
+        });
+        colors.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Context context = getApplicationContext();
+                Toast.makeText(context, "Opening Colors Page", Toast.LENGTH_SHORT).show();
+                Intent intent = new Intent(context, ColorsActivity.class);
+                startActivity(intent);
+            }
+        });
+        phrases.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Context context = getApplicationContext();
+                Toast.makeText(context, "Opening Phrases Page", Toast.LENGTH_SHORT).show();
+                Intent intent = new Intent(context, PhrasesActivity.class);
+                startActivity(intent);
+            }
+        });
+
     }
+    @Override
+    public void onClick(View v) {
+    }
+
 }
--- app/src/main/java/com/example/android/miwok/NumbersActivity.java
+++ app/src/main/java/com/example/android/miwok/NumbersActivity.java
@@ -3,6 +3,12 @@ package com.example.android.miwok;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.ViewGroup;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import java.util.ArrayList;
 
 public class NumbersActivity extends AppCompatActivity {
 
@@ -10,5 +16,25 @@ public class NumbersActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_numbers);
+        ArrayList<String> numbers = new ArrayList<>();
+        numbers.add("One");
+        numbers.add("Two");
+        numbers.add("Three");
+        numbers.add("Four");
+        numbers.add("Five");
+        numbers.add("Six");
+        numbers.add("Seven");
+        numbers.add("Eight");
+        numbers.add("Nine");
+        numbers.add("Ten");
+        LinearLayout linearLayout = findViewById(R.id.linearLayout);
+        for(int i=0;i<numbers.size();i++){
+            TextView tv = new TextView(this, null, 0, R.style.CategoryStyle);
+            tv.setLayoutParams(new LinearLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT));
+            tv.setText(numbers.get(i));
+            tv.setTextColor(0xFF040000);
+            tv.setBackgroundColor(0xFFFFD9A9);
+            linearLayout.addView(tv);
+        }
     }
 }
