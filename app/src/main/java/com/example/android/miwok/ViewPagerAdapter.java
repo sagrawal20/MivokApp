349700cb61eacca0cdca2c1371455ca7962852f5
saahil <saahil.agrawal20@gmail.com>
2023-01-05 00:29:30 +0530

    All Fragments made and Updated with Main activity.

---
+++ app/src/main/java/com/example/android/miwok/ViewPagerAdapter.java
@@ -0,0 +1,56 @@
+package com.example.android.miwok;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentPagerAdapter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ViewPagerAdapter extends FragmentPagerAdapter {
+
+    private final List<Fragment> fragments = new ArrayList<>();
+    private final List<String> fragmentTitle = new ArrayList<>();
+
+    public ViewPagerAdapter(@NonNull FragmentManager fm){
+        super(fm);
+    }
+
+    public void add(Fragment fragment, String title) {
+        fragments.add(fragment);
+        fragmentTitle.add(title);
+    }
+
+    @NonNull
+    @Override
+    public Fragment getItem(int position) {
+        switch (position){
+            case 0:
+                return new NumbersFragment();
+            case 1:
+                return new FamilyFragment();
+            case 2:
+                return new ColorsFragment();
+            case 3:
+                return new PhrasesFragment();
+            default:
+                return null;
+        }
+    }
+
+    @Override
+    public int getCount() {
+        return fragments.size();
+    }
+
+    @Nullable
+    @Override
+    public CharSequence getPageTitle(int position) {
+        return fragmentTitle.get(position);
+    }
+
+
+
+}
